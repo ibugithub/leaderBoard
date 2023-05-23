8c235e7d336d7649b5734076c830a4b90a7ec36e
ibugithub <outsideworkibrahim@gmail.com>
2023-05-23 13:09:13 +0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+import event from './eventHandler.js';
+
+event.handleTaskList();
+event.handleCheckClick();
+event.handleAddInputElement();
+event.handleRefressIcon();
+event.handleClearSelectedButton();
+event.handlingSelectedonRef();
+event.handlingEnterIconClick();
